b89bb4fea547c73f339b291cf92e4aaaba9f3769
adamsylla97 <209474@edu.p.lodz.pl>
2019-03-21 17:48:30 +0100

    testy_v1 *fixed code format

--- src/test/java/BinarySearchTests.java
+++ src/test/java/BinarySearchTests.java
@@ -6,81 +6,81 @@ import org.junit.jupiter.api.Test;
 public class BinarySearchTests {
 
     @Test
-    public void elementIsInSequenceLengthEqualsOne(){
+    public void elementIsInSequenceLengthEqualsOne() {
 
         int[] seq = {8};
         final int KEY = 8;
         final int EXPECTED_VALUE = 0;
 
-        SearchResult result = BinarySearch.search(KEY,seq);
+        SearchResult result = BinarySearch.search(KEY, seq);
 
-        Assertions.assertEquals(true,result.isFound());
-        Assertions.assertEquals(EXPECTED_VALUE,result.getPosition());
+        Assertions.assertEquals(true, result.isFound());
+        Assertions.assertEquals(EXPECTED_VALUE, result.getPosition());
     }
 
     @Test
-    public void elementIsNotInSequenceLengthEqualsOne(){
+    public void elementIsNotInSequenceLengthEqualsOne() {
 
         int[] seq = {3};
         final int KEY = 4;
         final int EXPECTED_VALUE = -1;
 
-        SearchResult result = BinarySearch.search(KEY,seq);
+        SearchResult result = BinarySearch.search(KEY, seq);
 
-        Assertions.assertEquals(false,result.isFound());
-        Assertions.assertEquals(EXPECTED_VALUE,result.getPosition());
+        Assertions.assertEquals(false, result.isFound());
+        Assertions.assertEquals(EXPECTED_VALUE, result.getPosition());
 
     }
 
     @Test
-    public void elementIsFirstElementOfSeq(){
+    public void elementIsFirstElementOfSeq() {
 
-        int[] seq = {1,2,3,4};
+        int[] seq = {1, 2, 3, 4};
         final int KEY = 1;
         final int EXPECTED_VALUE = 0;
 
-        SearchResult result = BinarySearch.search(KEY,seq);
+        SearchResult result = BinarySearch.search(KEY, seq);
 
-        Assertions.assertEquals(true,result.isFound());
-        Assertions.assertEquals(EXPECTED_VALUE,result.getPosition());
+        Assertions.assertEquals(true, result.isFound());
+        Assertions.assertEquals(EXPECTED_VALUE, result.getPosition());
 
     }
 
     @Test
-    public void elementIsLastElementofSeq(){
+    public void elementIsLastElementofSeq() {
 
-        int[] seq = {1,2,3,4};
+        int[] seq = {1, 2, 3, 4};
         final int KEY = 4;
-        final int EXPECTED_VALUE = seq.length-1;
+        final int EXPECTED_VALUE = seq.length - 1;
 
-        SearchResult result = BinarySearch.search(KEY,seq);
+        SearchResult result = BinarySearch.search(KEY, seq);
 
-        Assertions.assertEquals(true,result.isFound());
-        Assertions.assertEquals(EXPECTED_VALUE,result.getPosition());
+        Assertions.assertEquals(true, result.isFound());
+        Assertions.assertEquals(EXPECTED_VALUE, result.getPosition());
 
     }
 
     @Test
-    public void elementIsInTheMiddleOfSeq(){
+    public void elementIsInTheMiddleOfSeq() {
 
-        int[] seq = {1,2,3,4,5};
+        int[] seq = {1, 2, 3, 4, 5};
         final int KEY = 3;
-        final int EXPECTED_VALUE = seq.length/2;
+        final int EXPECTED_VALUE = seq.length / 2;
 
-        SearchResult result = BinarySearch.search(KEY,seq);
+        SearchResult result = BinarySearch.search(KEY, seq);
 
         Assertions.assertTrue(result.isFound());
-        Assertions.assertEquals(EXPECTED_VALUE,result.getPosition());
+        Assertions.assertEquals(EXPECTED_VALUE, result.getPosition());
 
     }
 
     @Test
-    public void elementIsNotInSequenceLengthEqualsMoreThanOne(){
+    public void elementIsNotInSequenceLengthEqualsMoreThanOne() {
 
-        int[] seq = {1,2,3,4};
+        int[] seq = {1, 2, 3, 4};
         final int KEY = 5;
 
-        SearchResult result = BinarySearch.search(KEY,seq);
+        SearchResult result = BinarySearch.search(KEY, seq);
 
         Assertions.assertFalse(result.isFound());
 
