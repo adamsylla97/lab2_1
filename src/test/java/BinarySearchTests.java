a6dfca164d9686e8851fd92d65b56384cad5d725
adamsylla97 <209474@edu.p.lodz.pl>
2019-03-21 17:19:34 +0100

    *prepared signatures of methods for futures tests

---
+++ src/test/java/BinarySearchTests.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+
+public class BinarySearchTests {
+
+    @Test
+    public void elementIsInSequenceLengthEqualsOne(){
+
+    }
+
+    @Test
+    public void elementIsNotInSequenceLengthEqualsOne(){
+
+    }
+
+    @Test
+    public void elementIsFirstElementOfSeq(){
+
+    }
+
+    @Test
+    public void elementIsLastElementofSeq(){
+
+    }
+
+    @Test
+    public void elementIsInTheMiddleOfSeq(){
+
+    }
+
+    @Test
+    public void elementIsNotInSequenceLengthEqualsMoreThanOne(){
+
+    }
+
+}
